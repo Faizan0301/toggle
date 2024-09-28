4796d8b380ac380ddf70d6334205e64ec256c385
Faizan Ansari <faizanrednwhite@gmail.com>
2024-09-29 01:59:30 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -15,44 +15,44 @@ function App() {
       <div className='container my-5 text-center'>
         <button onClick={ toggle } className='btn btn-lg btn-dark'>Toggle</button>
       </div>
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className={ `card ${color ? 'text-bg-danger' : ' text-bg-success'} mb-3 col-3 mx-2` }>
+      <div className="container" >
+        <div className="row justify-content-center" >
+          <div className={ `card ${color ? 'text-bg-danger' : ' text-bg-success'} mb-3 col-3 mx-2` } style={ { transition: "all .5s linear" } }>
             <div className="card-header">Header</div>
             <div className="card-body">
               <h5 className="card-title">Danger card title</h5>
               <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
           </div>
-          <div className={ `card ${color ? 'text-bg-success' : ' text-bg-dark'} mb-3 col-3 mx-2` }>
+          <div className={ `card ${color ? 'text-bg-success' : ' text-bg-dark'} mb-3 col-3 mx-2` } style={ { transition: "all .5s linear" } }>
             <div className="card-header">Header</div>
             <div className="card-body">
               <h5 className="card-title">Danger card title</h5>
               <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
           </div>
-          <div className={ `card ${color ? 'text-bg-dark' : ' text-bg-warning'} mb-3 col-3 mx-2` }>
+          <div className={ `card ${color ? 'text-bg-dark' : ' text-bg-warning'} mb-3 col-3 mx-2` } style={ { transition: "all .5s linear" } }>
             <div className="card-header">Header</div>
             <div className="card-body">
               <h5 className="card-title">Danger card title</h5>
               <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
           </div>
-          <div className={ `card ${color ? 'text-bg-warning' : ' text-bg-danger'} mb-3 col-3 mx-2` }>
+          <div className={ `card ${color ? 'text-bg-warning' : ' text-bg-danger'} mb-3 col-3 mx-2` } style={ { transition: "all .5s linear" } }>
             <div className="card-header">Header</div>
             <div className="card-body">
               <h5 className="card-title">Danger card title</h5>
               <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
           </div>
-          <div className={ `card ${color ? 'text-bg-danger' : ' text-bg-success'} mb-3 col-3 mx-2` }>
+          <div className={ `card ${color ? 'text-bg-danger' : ' text-bg-success'} mb-3 col-3 mx-2` } style={ { transition: "all .5s linear" } }>
             <div className="card-header">Header</div>
             <div className="card-body">
               <h5 className="card-title">Danger card title</h5>
               <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
           </div>
-          <div className={ `card ${color ? 'text-bg-success' : ' text-bg-dark'} mb-3 col-3 mx-2` }>
+          <div className={ `card ${color ? 'text-bg-success' : ' text-bg-dark'} mb-3 col-3 mx-2` } style={ { transition: "all .5s linear" } }>
             <div className="card-header">Header</div>
             <div className="card-body">
               <h5 className="card-title">Danger card title</h5>
